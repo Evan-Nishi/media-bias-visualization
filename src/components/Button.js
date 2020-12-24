57f2ca17004e5d856bcd9d29d41f174f74af8d78
Evan Nishi <evan@evannishi.com>
2020-12-23 17:34:12 -0800

    changed to function components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,18 @@
-import React from 'react'
 import styled from 'styled-components'
-export default class Button extends React.Component{
-    render(){
-        return(
-            <a className="button" href={this.props.href}>{this.props.children}</a>
-        )
-    }
-}
 
+const Button = styled.a`
+    -moz-user-select: none;
+    -webkit-user-select: none;
+    cursor: pointer;
+    background-color: white;
+    z-index: -2;
+    font-size: 2.2vw;
+    padding: .3vw;
+    height: 3vh;
+    width: 5vw;
+    border: 1px solid;
+    box-shadow: 5px 10px #666666;
+    transition: all 0.5s ease-out;
+`
+
+export default Button
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,39 +3,36 @@ import Layout from '../components/Layout'
 import Chart from 'react-google-charts'
 import Card from '../components/Card'
 
-export default class Index extends React.Component {
-    render(){
-        return(
-            <Layout>
-                <h1>Media Bias v Reliability v Viewership</h1>
-                <br/>
-                <Card>
-                    <p>
-                        This chart is an extention of the <a href="https://www.adfontesmedia.com/interactive-media-bias-chart-2/">ad fontes media chart</a>, which in addition to showing the reliability and partisianship of media websites, also shows viewership.
-                        This graph doesn't take into account tv viewer statistics, and only has website statistics from the months of 11/2020-12/2020.
-                        Data for reliability and bias was taken from <a href="https://www.adfontesmedia.com/">ad fontes media</a>.
-                        The raw data used in the graph can be found <a href="./data.csv">here</a>.
-                    </p>
-                </Card>
-                <br/>
-                <br/>
-                <Card>
-                    <Chart
-                        chartType="BubbleChart"
-                        spreadSheetUrl="https://docs.google.com/spreadsheets/d/1nbXs068fIUllVtV5OODayEZZds8T7sL2BcxCEiGpZBQ/edit#gid=555083803"
-                        width={'70vw'}
-                        height={'80vh'}
-                        options={{
-                            hAxis:{title: 'politcal bias', format:'short'},
-                            vAxis:{title: 'reliability', format:'decimal'},
-                            colorAxis: {colors:['blue','white','red']},
-                            bubble: {textStyle: {opacity: 0}}
-                        }}
-                    />
-                    Data last collected: 12/14/2020
-                </Card>
-            </Layout>
+export default function Index(){
+    return(
+        <Layout>
+            <h1>Media Bias v Reliability v Viewership</h1>
+            <br/>
+            <Card>
+                <p>
+                    This chart is an extention of the <a href="https://www.adfontesmedia.com/interactive-media-bias-chart-2/">ad fontes media chart</a>, which in addition to showing the reliability and partisianship of media websites, also shows viewership.
+                    This graph doesn't take into account tv viewer statistics, and only has website statistics from the months of 11/2020-12/2020.
+                    Data for reliability and bias was taken from <a href="https://www.adfontesmedia.com/">ad fontes media</a>.
+                    The raw data used in the graph can be found <a href="./data.csv">here</a>.
+                </p>
+            </Card>
+            <br/>
+            <br/>
+            <Card>
+                <Chart
+                    chartType="BubbleChart"
+                    spreadSheetUrl="https://docs.google.com/spreadsheets/d/1nbXs068fIUllVtV5OODayEZZds8T7sL2BcxCEiGpZBQ/edit#gid=555083803"
+                    width={'70vw'}
+                    height={'80vh'}
+                    options={{
+                        hAxis:{title: 'politcal bias', format:'short'},
+                        vAxis:{title: 'reliability', format:'decimal'},
+                        colorAxis: {colors:['blue','white','red']},
+                        bubble: {textStyle: {opacity: 0}}
+                    }}
+                />
+                Data last collected: 12/14/2020
+            </Card>
+        </Layout>
         )
-    }
 }
-//https://docs.google.com/spreadsheets/d/1nbXs068fIUllVtV5OODayEZZds8T7sL2BcxCEiGpZBQ/edit?usp=sharing
